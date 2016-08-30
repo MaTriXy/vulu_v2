2719386270570821a20aa31ae9c4014d7beab964
ELADLEVKOVITZ\Elad <greenlad24@gmail.com>
2016-08-30 14:05:58 +0300

    no message

--- client/src/common/filters/filters.module.js
+++ client/src/common/filters/filters.module.js
@@ -3,7 +3,7 @@ export default angular.module('si.filters', [])
 .filter('linkyWithHtml', function($filter) {
   return function(value) {
     if (value){
-      var linked = $filter('linky')(value);
+      var linked = $filter('linky')(value, '_blank');
       var replaced = linked.replace(/\&gt;/g, '>').replace(/\&lt;/g, '<');
     }
     return replaced;
